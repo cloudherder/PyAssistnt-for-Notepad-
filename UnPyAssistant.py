e1f50bffa4b4743817acded79880fecec238d250
cloudherder <cloudherder@126.com>
2015-06-01 19:56:30 +0800

    Create UnPyAssistant.py

---
+++ UnPyAssistant.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+#coding=utf-8
+#   Programmer: cloudherder
+#   E-mail:     cloudherder@126.com
+#
+#   Copyleft 2015  cloudherder
+#
+#   Distributed under the terms of the GPL (GNU Public License)
+#
+#   PyAssistant for Notepad++ is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+from Npp import *
+
+editor.clearCallbacks()
+notepad.clearCallbacks()
+console.write("\nPyAssistant has unhooked!\n")
